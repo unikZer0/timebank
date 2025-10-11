1f34e43121860401ca6ef42a65eca35b7e81fae0
bounhieng <Teng.ios123@gmail.com>
2025-10-11 14:01:36 +0700

    fix jobapp

--- src/routes/job_appRoutes.js
+++ src/routes/job_appRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import {postJobApp,getJobAppsByUser,updateJobAppStatus} from "../controllers/job_appController.js";
+import { authMiddleware } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
-router.post('/jobapp', postJobApp);
-router.get('/jobapp/user/:userId',getJobAppsByUser);
-router.put('/jobapp/:id/status',updateJobAppStatus);
+router.post('/jobapp', authMiddleware, postJobApp);
+router.get('/jobapp/user/:userId', authMiddleware, getJobAppsByUser);
+router.put('/jobapp/:id/status', authMiddleware, updateJobAppStatus);
 
 export default router;
